9dbaa73320e8c2c6c2d385f2474960b1c21d8bfb
Tim Sessanna <filefly@gmail.com>
2022-03-06 07:06:00 -0700

    Add sample config file

---
+++ config.sample.py
@@ -0,0 +1,19 @@
+"""
+config = {
+    "command_prefix": ".",
+    "owner_id": "discord_id_of_owner_goes_here",
+    "api_token": "discord_api_token_goes_here",
+    "github_repo_url": "https://github.com/url/to/repo",
+    "bot_name": "YouTubeMusicBot",
+    "bot_version": "1.0.0"
+}
+"""
+
+config = {
+    "command_prefix": "",
+    "owner_id": "",
+    "api_token": "",
+    "github_repo_url": "",
+    "bot_name": "",
+    "bot_version": ""
+}
